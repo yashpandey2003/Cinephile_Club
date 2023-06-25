6f01e7b7f0f5d4f61667379fcc59616b3d8617f1
yashpandey2003 <yashisro@gmail.com>
2023-06-25 18:00:07 +0530

    final project

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+import logo from "./Cinephile Club-logos.jpeg"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src={logo} /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
